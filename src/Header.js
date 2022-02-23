4d6c0541e0b282346877d21dae4c47a566069bbc
luisruarios <luis.rua@guarapo.dev>
2022-02-23 10:43:30 -0500

    Create-header

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import HeaderOption from './HeaderOption';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 const Header = () => {
     return (
@@ -15,9 +21,15 @@ const Header = () => {
             </div>
 
             <div className="header__right">
+                <HeaderOption Icon={HomeIcon} title='Home' />
+                <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
 
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+                <HeaderOption Icon={ChatIcon} title='Messaging' />
+                <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+                <HeaderOption avatar='https://www.stockvault.net//data/2018/08/28/254043/thumb16.jpg'
+                    title='me' />
             </div>
-
         </div>
     )
 }
